6acc0e29f6594c23d200f3685c346bcc41c4d164
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-12-24 09:45:30 -0800

    undid rust based version

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,17 +9,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_wallpaper::init())
-        .setup(|app| {
-            let app_handle = app.handle();
-            let webview_window = app_handle.get_webview_window("wallpaper").unwrap();
-
-            app_handle
-                .wallpaper()
-                .attach_window(&webview_window)
-                .unwrap();
-            
-            Ok(())
-        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
